7d48aaa279ff3f86ba97f66a4558afc1d1b4f688
Harvey <harveyspec46@gmail.com>
2026-06-02 16:20:35 -0700

    Sync build environment configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
-404: Not Found
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
